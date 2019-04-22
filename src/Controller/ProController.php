fd1defaf25d7776d8baeb4b1ed50573c573cfb70
Jonathan <jon.renault@protonmail.com>
2019-04-22 15:31:18 +0200

    Ajout en formulaire des coordonnées EC

--- src/Controller/ProController.php
+++ src/Controller/ProController.php
@@ -33,6 +33,39 @@ class ProController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
+            if($proProfil->getAdresse() != $form->get("adresse") || $proProfil->getCodepostal() != $form->get("codepostal") || $proProfil->getVille() != $form->get("ville"))
+            {
+                $street = $form->get("adresse");
+                $ville = $form->get("ville");
+                $codepostal = $form->get("codepostal");
+
+                $adress = array(
+                    'street'    => $street,
+                    'city'      => $ville,
+                    'postalcode'=>  $codepostal,
+                    'country'   =>  'France',
+                    'format'    =>  'json'
+                );
+                // Création d'une nouvelle ressource cURL
+                $ch = curl_init("https://nominatim.openstreetmap.org/?". http_build_query($adress));
+
+                // Configuration de l'URL et d'autres options
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_USERAGENT, 'Mettre ici un user-agent adéquat');
+
+                // Récupération de l'URL et affichage sur le navigateur
+                $g = curl_exec($ch);
+
+                // Fermeture de la session cURL
+                curl_close($ch);
+
+                $json_data = json_decode($g,true);
+
+                $proProfil->setLatitude($json_data[0]['lat']);
+                $proProfil->setLongitude($json_data[0]['lon']);
+
+            }
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($proProfil);
             $em->flush();
